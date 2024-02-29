79d57596c09cb319e45a0007a036a8c68041cf79
vutlajava <vutlajava@gmail.com>
2024-02-28 22:08:16 -0600

    angular load image programe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'imageload';
+  @Input() loader:string='https://media.tenor.com/images/f864cbf3ea7916572605edd3b3fe637f/tenor.gif';
+  @Input() height:number=200;
+  @Input() width:number=200;
+  @Input() image:string ='https://picsum.photos/400?image=179';
+
+  isLoading:boolean;
+  
+  constructor() { 
+    this.isLoading=true;
+  }
+
+  hideLoader(){
+    this.isLoading=false;
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration()]
